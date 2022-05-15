08b3f470f7c153a7fc7b762d0cbe5e3b3959e734
Prasad Dhanikonda <prasadsource@gmail.com>
2022-05-16 00:25:55 +0530

    Added git demo project2 local repo

---
+++ src/main/java/com/example/demo/PrasadGroupApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrasadGroupApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrasadGroupApp2Application.class, args);
+	}
+
+}
