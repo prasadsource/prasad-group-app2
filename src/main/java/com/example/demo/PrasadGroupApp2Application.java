ce52ce6949cb80e54c8c31549dce8728150537dd
Prasad Dhanikonda <prasadsource@gmail.com>
2022-05-16 00:29:55 +0530

    added rest controller wish

--- src/main/java/com/example/demo/PrasadGroupApp2Application.java
+++ src/main/java/com/example/demo/PrasadGroupApp2Application.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PrasadGroupApp2Application {
 
+	@GetMapping("/testMessage")
+	public String getWishes() {
+		return "Hello Prasad Group Chairman...App2";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrasadGroupApp2Application.class, args);
 	}
